39d69db76d463e67e37229b8218d8a7a2997312d
Soham Kukreti <kukretisoham@gmail.com>
2023-04-22 16:12:17 +0530

    Created basic structure for game

---
+++ urquQuestion.py
@@ -0,0 +1,43 @@
+import pygame
+import sys
+def prompt1():
+
+	# initialize Pygame
+	pygame.init()
+
+	# set the window size
+	window_size = (1000, 1000)
+
+	# create the window
+	window = pygame.display.set_mode(window_size)
+
+	# set the font
+	font = pygame.font.SysFont(None, 30)
+
+	question_img = pygame.image.load("img/urdu/q1.png")
+
+
+	bgimg = pygame.image.load("img/bgimg.png")
+
+
+	# main game loop
+	running = True
+	while running:
+		# handle events
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				# check if the user clicked on an option
+				mouse_pos = pygame.mouse.get_pos()
+				option_y = 100
+
+		# clear the screen
+		window.fill((255, 255, 255))
+		window.blit(bgimg,(0,0))
+		window.blit(question_img,(150,0))
+		# update the screen
+		pygame.display.update()
+
+
+prompt1()
