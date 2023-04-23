ec86b9b8297724b5b44265c8f7f2ad8e8e79fee4
Soham Kukreti <kukretisoham@gmail.com>
2023-04-23 12:59:17 +0530

    organized files and modified language menu

--- help_menu.py
+++ help_menu.py
@@ -9,14 +9,14 @@ pygame.display.set_caption('How to Play')
 
 def help_call():
     # Load the help page image
-                help_page_img = pygame.image.load('help_page.png')
+                help_page_img = pygame.image.load('menuAssets/help_page.png')
                 screen.blit(help_page_img, (0, 0))
                 pygame.display.update()
                 # Wait for the user to go back to the main menu
                 back_flag = False
                 while not back_flag:
                     # Draw the back button
-                    back = pygame.image.load('back.png')
+                    back = pygame.image.load('menuAssets/back.png')
                     back_button_rect = pygame.Rect(20, 20, back.get_width(), back.get_height())
                     screen.blit(back, back_button_rect)
                     # back_button_rect = pygame.Rect(20, 20, 100, 50)
