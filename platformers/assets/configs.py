7d8d7b65dade6f64711a9db01bd6b5350f3be9bd
Dhanvith Nayak <dhanpran3@gmail.com>
2024-01-05 23:29:26 +0530

    deleted dump.py and rearranged levels

--- platformers/assets/configs.py
+++ platformers/assets/configs.py
@@ -13,7 +13,7 @@ class PlatformerConfig:
     world_data: list = None
     font: pygame.font = pygame.font.SysFont("Bauhaus 93", 70)
     font_score: pygame.font = pygame.font.SysFont("Bauhaus 93", 30)
-    martyimg_time: int = 600 #################
+    martyimg_time: int = 600 
 
 
 @dataclass
